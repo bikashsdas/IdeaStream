58cddd2e3a68a018c9dc301284be964399eb68c9
Bikash Sukla Das <bikashsukladas@macbook-pro.in.ibm.com>
2017-08-17 17:52:09 +0530

    updated code for login and home controller and also in storyboard

--- FasttrackSale/FasttrackSale/LoginViewController.swift
+++ FasttrackSale/FasttrackSale/LoginViewController.swift
@@ -27,6 +27,35 @@ class LoginViewController: UIViewController,UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        tfUsername?.text = nil
+        tfPassword?.text = nil
+    }
+    
+    //User login functional validation
+    
+    @IBAction func btnLoginPressed(_sender: UIButton){
+        
+        if(tfUsername?.text == "abc@gmail.com") && (tfPassword?.text == "123456"){
+            
+             let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+             let homeControllerObj = storyBoard.instantiateViewController(withIdentifier: "HomeControllerSBID") as! HomeViewController
+             self.navigationController?.pushViewController(homeControllerObj, animated: true)
+            
+        }
+        else{
+            
+             let alertObj = UIAlertController(title: "Alert", message: "Invalid credentials! Please enter the correct credentials.", preferredStyle: UIAlertControllerStyle.alert)
+             alertObj.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+             self.present(alertObj, animated: true, completion: nil)
+            
+        }
+    }
+    
+    
+    
     //MARK:- UITextField delegate methods
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
